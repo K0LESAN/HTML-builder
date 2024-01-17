bd8a1e2dad367ab83f4758e1d56ed0ebcdcc3f1c
C0lgate <k0lesan2k00@gmail.com>
2024-01-17 11:25:33 +0300

    refactor: rename read stream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 const fullpath = path.join(__dirname, 'text.txt');
-const stream = fs.createReadStream(fullpath, { encoding: 'utf-8' });
+const readStream = fs.createReadStream(fullpath, { encoding: 'utf-8' });
 
 function outputData(data) {
   process.stdout.write(data);
 }
 
-stream.on('data', outputData);
+readStream.on('data', outputData);
